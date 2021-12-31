3c866c8eb590eba6246b1e81730f3072fe920fd5
kristophertope <78042859+kristophertope@users.noreply.github.com>
2021-12-31 13:56:04 -0600

    Added basic responsiveness to expenses and debt

--- debt_app/views.py
+++ debt_app/views.py
@@ -14,55 +14,57 @@ from bokeh.transform import cumsum
 
 # Create your views here.
 def debt(request):
+              
+       ##TABLE##
+       debt_table = debt_item.objects.all()
+       total_debt = debt_item.objects.aggregate(Sum('debt_balance'))
+       total_debt = round(total_debt.get('debt_balance__sum'), 2)
 
-    ##TABLE##
-    debt_table = debt_item.objects.all()
-    total_debt = debt_item.objects.aggregate(Sum('debt_balance'))
-    total_debt = round(total_debt.get('debt_balance__sum'), 2)
+       #INTEREST PER MONTH
+       debt_table_dict = debt_table.values()
 
-    #INTEREST PER MONTH
-    debt_table_dict = debt_table.values()
+       debt_balance_list = [a_dict['debt_balance'] for a_dict in debt_table_dict]
+       debt_interest_list = [a_dict['debt_interest_rate'] for a_dict in debt_table_dict]
+       debt_im_list = []
 
-    debt_balance_list = [a_dict['debt_balance'] for a_dict in debt_table_dict]
-    debt_interest_list = [a_dict['debt_interest_rate'] for a_dict in debt_table_dict]
-    debt_im_list = []
+       #for i in debt_balance_list:
+       #   debt_im_list[i] = debt_balance_list[i]
+       
+       
 
-    #for i in debt_balance_list:
-     #   debt_im_list[i] = debt_balance_list[i]
-    
-    
+       print(debt_balance_list)
+       print(debt_interest_list)
 
-    print(debt_balance_list)
-    print(debt_interest_list)
+       ##CHART##
+       piechart_data = list(debt_item.objects.all().values())
+       names_list = [i["debt_name"] for i in piechart_data]
+       balance_list = [i["debt_balance"] for i in piechart_data]
+       piechart_data_dict = dict(zip(names_list, balance_list))
 
-    ##CHART##
-    piechart_data = list(debt_item.objects.all().values())
-    names_list = [i["debt_name"] for i in piechart_data]
-    balance_list = [i["debt_balance"] for i in piechart_data]
-    piechart_data_dict = dict(zip(names_list, balance_list))
+       data = pd.Series(piechart_data_dict).reset_index(name='value').rename(columns={'index':'category'})
+       data['angle'] = data['value']/data['value'].sum() * 2*pi
+       data['color'] = Greys[len(piechart_data_dict)]
 
-    data = pd.Series(piechart_data_dict).reset_index(name='value').rename(columns={'index':'category'})
-    data['angle'] = data['value']/data['value'].sum() * 2*pi
-    data['color'] = Greys[len(piechart_data_dict)]
+       p = figure(plot_height=400, plot_width=400, title="", toolbar_location=None,
+              tools="hover", tooltips="@category: @value", x_range=(-0.5, 1.0))
+       #p.sizing_mode = "scale_both"
+       p.min_border = 0
 
-    p = figure(plot_height=550, plot_width=400, title="", toolbar_location=None,
-           tools="hover", tooltips="@category: @value", x_range=(-0.5, 1.0))
+       p.wedge(x=0, y=1, radius=0.4,
+       start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+       line_color="black", fill_color='color', legend_field='category', source=data)
 
-    p.wedge(x=0, y=1, radius=0.4,
-    start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
-    line_color="black", fill_color='color', legend_field='category', source=data)
+       p.axis.axis_label=None
+       p.axis.visible=False
+       p.grid.grid_line_color = None
+       p.legend.location = "right"
+       p.legend.background_fill_color = None
+       p.outline_line_color = None
+       p.border_fill_color = None
+       p.background_fill_color = None
 
-    p.axis.axis_label=None
-    p.axis.visible=False
-    p.grid.grid_line_color = None
-    p.legend.location = "bottom_right"
-    p.legend.background_fill_color = None
-    p.outline_line_color = None
-    p.border_fill_color = None
-    p.background_fill_color = None
+       #p.x_range.range_padding = 0.02
 
-    #p.x_range.range_padding = 0.02
+       script, div = components(p, CDN)
 
-    script, div = components(p, CDN)
-
-    return render(request, "debt.html", locals())
+       return render(request, "debt.html", locals())
